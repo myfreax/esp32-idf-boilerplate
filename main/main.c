042fc904f0fb0d5419a89fff52ef76e7047b84a2
myfreax <web@myfreax.com>
2024-03-28 11:38:09 +0800

    feat: commit categories

--- main/main.c
+++ main/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-void app_main(void)
-{
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
+static const char* TAG = "RELEASE";
+
+void app_main(void) {
+  while (1) {
+    ESP_LOGI(TAG, "feat");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
 }
